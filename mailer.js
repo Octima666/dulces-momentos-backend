d89e0ad5d3f5ed238a31d790ea03c7687ed9a560
JoaLugo <joalugo45@gmail.com>
2026-09-17 15:41:52 -0300

    Configure TLS for mailer transport
    Added TLS configuration to the mailer transport.

--- mailer.js
+++ mailer.js
@@ -12,10 +12,13 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 465,
-  secure: true,
+  secure: true, // true para puerto 465
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
   }
 });
 
